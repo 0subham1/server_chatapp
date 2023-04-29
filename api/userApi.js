a621dfd1a86866075f8c01bcfc5a549fc06d2f2b
Subham Gupta <0subhamgupta1@gmail.com>
2023-04-29 17:38:03 +0530

    a

--- api/userApi.js
+++ api/userApi.js
@@ -1,7 +1,9 @@
 const express = require("express");
 const router = express.Router();
 const users = require("../db/userModel");
-
+const cors = require("cors");
+app.use(cors());
+app.use(express.json());
 router.post("/signup", async (req, res) => {
   let obj = {
     name: req.body.name,
