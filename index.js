62bdccf4614894433721287265952a251ca89056
subham gupta <0subhamgupta1@gmail.com>
2023-08-03 13:30:57 +0530

    a

--- index.js
+++ index.js
@@ -17,16 +17,18 @@ app.use(passport.initialize());
 
 const jwt = require("jsonwebtoken");
 
-app.listen(port);
 const User = require("./model/chatUserModel");
 const Msg = require("./model/chatMsgModel");
+
+const errorHandler = require("./middleware/error");
 
 app.post("/register", async (req, res) => {
   let obj = {
     name: req.body.name,
     phone: Number(req.body.phone),
     password: req.body.password.toString(),
-    image: req.body.name,
+    isAdmin: false,
+    createdAt: new Date(),
   };
   let exist = await User.findOne({ name: req.body.name });
   console.log(exist, "exist");
@@ -39,15 +41,11 @@ app.post("/register", async (req, res) => {
   } else if (!obj.password) {
     errorHandler(res, 400, "Please enter password");
   } else {
-    let result = await User
-      .create(obj)
-      .then(() => {
-        console.log(result, "result");
-        res.status(201).send({ success: true, message: "user created" });
-      })
-      .catch((err) => {
-        errorHandler(res, 500, "server err");
-        console.log("err", err);
-      });
+    let result = await User.create(obj);
+    console.log(result, "result");
+    res.status(201).send({ success: true, message: "user created" });
   }
 });
+
+console.log("hello");
+app.listen(port);
